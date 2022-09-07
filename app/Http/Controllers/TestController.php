17b6ef26480b0cc201e577190e41fb1fd53a7a1a
zl_php <lei_0668@sina.com>
2022-09-07 10:43:47 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,8 +9,16 @@ class TestController extends Controller
 {
     public function test()
     {
-        echo 1;
+        $name = 'zhiguodu(xxxx）';
 
-        //throw new InvalidRequestException('参数不能为空');
+        // 1.不管有没有括号，无差别替换中文括号至英文括号
+        $relname = str_replace(['（', '）'], ['(', ')'], $name);
+
+        // 2.如果有括号，正则匹配括号里的名字
+        if(preg_match_all("/(?:\()(.*)(?:\))/i",$relname, $result))
+            $relname =  $result[1][0];
+
+        echo $relname;
+
     }
 }
